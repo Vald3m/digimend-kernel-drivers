9313240145f0b893c39d9735e6bc4037655f54db
Vald3m <4178272+Vald3m@users.noreply.github.com>
2020-04-07 13:35:45 +0400

    move frame button codes to BTN_2 to BTN_5 range

--- hid-hanwang.c
+++ hid-hanwang.c
@@ -126,10 +126,10 @@ __u8 hanwang_rdesc_parblo_a609_fixed[] = {
 	0x25, 0x01,         /*          Logical Maximum (1),        */
 	0x05, 0x09,         /*          Usage Page (Button),        */
 	0x75, 0x01,         /*          Report Size (1),            */
-	0x95, 0x10,         /*          Report Count (10),          */
+	0x95, 0x10,         /*          Report Count (16),          */
 	0x81, 0x03,         /*          Input (Constant, Variable), */
-	0x19, 0x10,         /*          Usage Minimum (10h),        */
-	0x29, 0x13,         /*          Usage Maximum (13h),        */
+	0x19, 0x03,         /*          Usage Minimum (03h),        */
+	0x29, 0x06,         /*          Usage Maximum (06h),        */
 	0x95, 0x04,         /*          Report Count (4),           */
 	0x81, 0x02,         /*          Input (Variable),           */
 	0x95, 0x34,         /*          Report Count (52),          */
